ca295cd333f4f29160ea16f9063bc4a756e6cfbb
Christopher Harrison <cnharrison@gmail.com>
2019-03-23 13:07:18 -0400

    refactored header to use styled components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React from "react";
+import styled, { css } from "styled-components";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+      width: 1280px;
+`;
+
+const StyledH1 = styled.div`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const StyledSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+    <StyledHeader>
+      <StyledSpan>SMARCH 32, 2018</StyledSpan>
+      <StyledH1>Lambda Times</StyledH1>
+      <StyledSpan>98°</StyledSpan>
+    </StyledHeader>
+  );
+};
 
-export default Header
+export default Header;
